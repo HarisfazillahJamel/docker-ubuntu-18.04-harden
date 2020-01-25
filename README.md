9591a10e05f58c8d6ea1a971aedb4c369f09d90e
Harisfazillah Jamel <linuxmalaysia@gmail.com>
2020-01-25 08:45:05 +0800

    First commit template from 14.04

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# docker-ubuntu-18.04-harden
-Docker Ubuntu harden for security with SSH OpenSSH (Training Docker)
+# Docker Ubuntu 18.04 Harden With SSH - OpenSSH
+
+Docker Container With Ubuntu 18.04 LTS harden and secure. Include SSH OpenSSH to access the server. The default user is user1 and the password in the latest builds details log. Visit this website for latest information about this docker and latest builds details.
+
+This docker is installed with :-
+
+- OpenSSH
+- Fail2ban
+- UFW with 22/tcp allow and default deny
+- Harden and harden-nids packages
+- docker run --privileged=true is needed by iptables
+
+Latest information in the docker/Dockerfile and hardening[.]sh
+
+### To Install On Your Server
+```sh
+docker pull linuxmalaysia/docker-ubuntu-18.04-harden
+```
+```sh
+docker run --privileged=true -it -d -P --name my_ubuntu1 linuxmalaysia/docker-ubuntu-18.04-harden
+```
+You can then use docker port to find out what host port, the container's port 22 is mapped to :-
+```sh
+docker port my_ubuntu1 port 22
+```
+```sh
+ssh user1@localhost -p ?????
+```
+### To build yourself
+```sh
+git clone https://github.com/HarisfazillahJamel/docker-ubuntu-14.04-harden.git
+```
+```sh
+cd docker-ubuntu-14.04-harden
+```
+```sh
+docker build -t docker-ubuntu-18.04-harden .
+```
+```sh
+docker run --privileged=true -it -d -P --name my_ubuntu1 docker-ubuntu-18.04-harden
+```
+haris @ Harisfazillah Jamel @ LinuxMalaysia
+
+25 Jan 2020
---
+++ hardening.sh
@@ -0,0 +1,69 @@
+# Harisfazillah Jamel
+# 21 July 2015
+# Origin: https://github.com/HarisfazillahJamel/docker-ubuntu-14.04-harden
+
+#
+# Copyright (c) 2015-2020 Harisfazillah Jamel <linuxmalaysia@gmail.com>
+#
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# This is my development scripts to harden server. Im using this docker to
+# applied hardening scripts. Use this Dockerfile and scripts related at your own risk.
+# Harisfazillah Jamel - Kuala Lumpur - Malaysia - 25 Jan 2015 - 25 Jan 2020
+
+# need to restart rsyslog
+
+service rsyslog stop
+service rsyslog start
+
+logger -it ujian "test the logger"
+
+# Need to restart cron
+
+service cron restart
+
+# Need to restart fail2ban
+# fail2ban.sock need to be deleted
+
+service fail2ban stop
+rm /var/run/fail2ban/fail2ban.sock
+service fail2ban start
+
+# Need to restart ssh and stop to initalize files
+
+service ssh restart
+service ssh stop
+
+# docker run --privileged=true need to be used for iptables related.
+
+ufw allow 22/tcp
+ufw enable
+ufw status
+ufw default deny
+
+# Crete SSH Key
+
+ssh-keygen -t rsa -f ~/.ssh/id_rsa -q -N ""
+
+# restart snort
+
+service snort restart
+
+#### Only ssh daemon will be the last execute
+# SSH run in foreground and not detached
+
+/usr/sbin/sshd -D
+
+exit 0
